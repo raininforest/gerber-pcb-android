6d4d49ef4ee0fc9033e02f69e4babcfef140c866
Sergey <s.velesko@yandex.ru>
2021-03-30 19:02:09 +0300

    add preferences

---
+++ app/src/main/java/com/github/raininforest/gerberpcb/view/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.github.raininforest.gerberpcb.view
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.github.raininforest.gerberpcb.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
