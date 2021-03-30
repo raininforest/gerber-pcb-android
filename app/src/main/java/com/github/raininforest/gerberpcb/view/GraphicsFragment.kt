cdbf9908a05a0fc2b1681b312639ae62c67eefce
Sergey <s.velesko@yandex.ru>
2021-03-30 19:03:23 +0300

    add graphics fragment with vm and layout

---
+++ app/src/main/java/com/github/raininforest/gerberpcb/view/GraphicsFragment.kt
@@ -0,0 +1,33 @@
+package com.github.raininforest.gerberpcb.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.raininforest.gerberpcb.R
+import com.github.raininforest.gerberpcb.viewmodel.GraphicsViewModel
+
+class GraphicsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = GraphicsFragment()
+    }
+
+    private lateinit var viewModel: GraphicsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.graphics_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(GraphicsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
