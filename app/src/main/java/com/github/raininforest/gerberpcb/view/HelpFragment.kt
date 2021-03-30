678c3f63ed19f6e3b7efc2fac5437e53f71b0c72
Sergey <s.velesko@yandex.ru>
2021-03-30 19:05:15 +0300

    add help fragment with vm and layout

---
+++ app/src/main/java/com/github/raininforest/gerberpcb/view/HelpFragment.kt
@@ -0,0 +1,33 @@
+package com.github.raininforest.gerberpcb.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.raininforest.gerberpcb.R
+import com.github.raininforest.gerberpcb.viewmodel.HelpViewModel
+
+class HelpFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HelpFragment()
+    }
+
+    private lateinit var viewModel: HelpViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.help_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HelpViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
