d8673aa54b43722ec21400549668807720ca3427
Sergey <s.velesko@yandex.ru>
2021-03-30 16:03:45 +0300

    set toolbar

--- app/src/main/java/com/github/raininforest/gerberpcb/MainActivity.kt
+++ app/src/main/java/com/github/raininforest/gerberpcb/MainActivity.kt
@@ -2,10 +2,18 @@ package com.github.raininforest.gerberpcb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.raininforest.gerberpcb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.inflateMenu(R.menu.main_menu)
     }
+
 }
