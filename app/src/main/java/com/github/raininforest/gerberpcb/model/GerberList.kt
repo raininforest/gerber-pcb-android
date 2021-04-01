be1c585f54494dda08fc23c74502f5e36bcc367a
Sergey <s.velesko@yandex.ru>
2021-04-01 19:38:03 +0300

    GerberList implementation

--- app/src/main/java/com/github/raininforest/gerberpcb/model/GerberList.kt
+++ app/src/main/java/com/github/raininforest/gerberpcb/model/GerberList.kt
@@ -1,4 +1,27 @@
 package com.github.raininforest.gerberpcb.model
 
-class GerberList {
+class GerberList() {
+    private val list: MutableList<GerberItem> = mutableListOf()
+
+    fun add(item: GerberItem) {
+        list.add(item)
+    }
+
+    fun remove(item: GerberItem) {
+        list.remove(item)
+    }
+
+    fun clear() {
+        list.clear()
+    }
+
+    fun getList(): List<GerberItem> {
+        return list
+    }
+
+    fun get(position: Int): GerberItem {
+        return list[position]
+    }
+
+    fun getSize(): Int = list.size
 }
