75984af79302ed83326ed85ade624234d8d732de
Sergey <s.velesko@yandex.ru>
2021-04-18 13:28:11 +0300

    show some test data

--- app/src/main/java/com/github/raininforest/gerberpcb/viewmodel/LayersViewModel.kt
+++ app/src/main/java/com/github/raininforest/gerberpcb/viewmodel/LayersViewModel.kt
@@ -3,33 +3,40 @@ package com.github.raininforest.gerberpcb.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.raininforest.gerberpcb.model.DataService
+import com.github.raininforest.gerberpcb.model.DataServiceImpl
 
 class LayersViewModel : ViewModel() {
     private val liveData: MutableLiveData<LayersScreenState> = MutableLiveData()
+    private val dataService: DataService = DataServiceImpl
 
     fun getLiveData(): LiveData<LayersScreenState> {
         return liveData
-        //TODO repository.getGerberList()
     }
 
-    fun addFile(gerberUri: String) {
+    fun getList() {
+        liveData.value = LayersScreenState.Success(dataService.getList())
+    }
+
+    fun addItem(gerberUri: String) {
         liveData.value = LayersScreenState.Loading
         //TODO repository.add(gerberUri). должен вернуть новый увеличенный список в лайвдату
     }
 
-    fun setItemVisible(gerberName: String, visible: Boolean) {
+    fun setItemVisibility(gerberName: String, visible: Boolean) {
         //TODO repository.setVisible(gerberName)
-    }
-
-    fun setItemColor(gerberName: String, color: String) {
-        //TODO setItemColor(gerberName: String, color: String)
     }
 
     fun removeItem(gerberName: String) {
         //TODO repository.remove(gerberName). должен вернуть новый уменьшенный список в лайвдату
     }
 
-    fun clear() {
+    fun clearList() {
         //TODO repository.clear(). должен вернуть пустоту
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        dataService.removeListeners()
     }
 }
