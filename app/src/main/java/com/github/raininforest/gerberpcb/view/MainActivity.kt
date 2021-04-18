c34d01d905f0750637df011e8501aac0273489d6
Sergey <s.velesko@yandex.ru>
2021-04-18 14:01:38 +0300

    migrate to Navigation Architecture Component

--- app/src/main/java/com/github/raininforest/gerberpcb/view/MainActivity.kt
+++ app/src/main/java/com/github/raininforest/gerberpcb/view/MainActivity.kt
@@ -3,24 +3,19 @@ package com.github.raininforest.gerberpcb.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.github.raininforest.gerberpcb.R
 import com.github.raininforest.gerberpcb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var currentScreen: ScreenName = ScreenName.NULL
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         initToolbar()
-        if (savedInstanceState == null) {
-            openScreen(ScreenName.LAYERS)
-        }
     }
 
     private fun initToolbar() {
@@ -31,39 +26,22 @@ class MainActivity : AppCompatActivity() {
     private val menuItemListener: (MenuItem) -> Boolean = {
         when (it.itemId) {
             R.id.action_layers -> {
-                openScreen(ScreenName.LAYERS)
+                findNavController(R.id.my_nav_host_fragment).navigate(R.id.layersFragment)
                 true
             }
             R.id.action_graphics -> {
-                openScreen(ScreenName.GRAPHICS)
+                findNavController(R.id.my_nav_host_fragment).navigate(R.id.graphicsFragment)
                 true
             }
             R.id.action_settings -> {
-                openScreen(ScreenName.SETTINGS)
+                findNavController(R.id.my_nav_host_fragment).navigate(R.id.settingsFragment)
                 true
             }
             R.id.action_help -> {
-                openScreen(ScreenName.HELP)
+                findNavController(R.id.my_nav_host_fragment).navigate(R.id.helpFragment)
                 true
             }
             else -> false
         }
-    }
-
-    private fun openScreen(screen: ScreenName) {
-        if (currentScreen == screen) return
-
-        val fragmentToSet: Fragment = when (screen) {
-            ScreenName.LAYERS -> LayersFragment.newInstance()
-            ScreenName.GRAPHICS -> GraphicsFragment.newInstance()
-            ScreenName.SETTINGS -> SettingsFragment()
-            ScreenName.HELP -> HelpFragment()
-            else -> return
-        }
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.mainContainer, fragmentToSet)
-            .commit()
-        currentScreen = screen
     }
 }
