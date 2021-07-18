87eb1afccf40c0983c8eb8fa43801baa80a2e780
Sergey Velesko <s.velesko@yandex.ru>
2021-07-18 15:22:12 +0300

    added application for hilt

---
+++ app/src/main/java/com/github/raininforest/gerberpcb/app/GerberReaderApp.kt
@@ -0,0 +1,10 @@
+package com.github.raininforest.gerberpcb.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Sergey Velesko on 18.07.2021
+ */
+@HiltAndroidApp
+class GerberReaderApp : Application()
