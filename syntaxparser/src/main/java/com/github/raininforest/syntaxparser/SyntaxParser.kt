e4f78aa2e1ea2fb5543a90ecf73f23e2051df84f
Sergey Velesko <s.velesko@yandex.ru>
2021-09-19 13:03:42 +0300

    created syntax parser api

--- syntaxparser/src/main/java/com/github/raininforest/syntaxparser/SyntaxParser.kt
+++ syntaxparser/src/main/java/com/github/raininforest/syntaxparser/SyntaxParser.kt
@@ -1,4 +1,10 @@
 package com.github.raininforest.syntaxparser
 
-class SyntaxParser {
+import com.github.raininforest.core.GerberCommand
+
+/**
+ * Created by Sergey Velesko on 19.09.2021
+ */
+interface SyntaxParser {
+    fun parse(stringList: List<String>): List<GerberCommand>
 }
---
+++ syntaxparser/src/main/java/com/github/raininforest/syntaxparser/SyntaxParserImpl.kt
@@ -0,0 +1,9 @@
+package com.github.raininforest.syntaxparser
+
+import com.github.raininforest.core.GerberCommand
+
+class SyntaxParserImpl: SyntaxParser {
+    override fun parse(stringList: List<String>): List<GerberCommand> {
+        TODO("Not yet implemented")
+    }
+}
