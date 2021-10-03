841ab6ac7e7627b75f902f4a7e499adb7f1a84ea
Sergey Velesko <s.velesko@yandex.ru>
2021-10-03 15:24:38 +0300

    add some checks to LineIndexHandler and covered by tests

--- syntaxparser/src/main/java/com/github/raininforest/syntaxparser/LineIndexHandler.kt
+++ syntaxparser/src/main/java/com/github/raininforest/syntaxparser/LineIndexHandler.kt
@@ -5,6 +5,16 @@ package com.github.raininforest.syntaxparser
  * Created by Sergey Velesko on 26.09.2021
  */
 class LineIndexHandler(private val maxIndex: Int) {
+
+    init {
+        if (maxIndex < 0) {
+            throw IllegalStateException("Wrong max line index. It should be >= 0")
+        }
+        if (maxIndex > Integer.MAX_VALUE) {
+            throw IllegalStateException("Wrong max line index. It should be <= 2147483647")
+        }
+    }
+
     private var lineNumber: Int = 0
 
     fun index() = lineNumber
---
+++ syntaxparser/src/test/java/common/LineIndexHandlerTest.kt
@@ -0,0 +1,48 @@
+package common
+
+import com.github.raininforest.syntaxparser.LineIndexHandler
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Test for [LineIndexHandler]
+ *
+ * Created by Sergey Velesko on 03.10.2021
+ */
+class LineIndexHandlerTest {
+    @Test
+    fun `test ok maxIndex`() {
+        val maxIndex = 679806
+        val indexHandler = LineIndexHandler(maxIndex)
+        for (i in 0 until maxIndex) {
+            indexHandler.increment()
+        }
+        Assert.assertEquals(indexHandler.index(), maxIndex)
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun `test illegal index increment`() {
+        val maxIndex = 2147483647
+        val indexHandler = LineIndexHandler(maxIndex)
+        for (i in 0..maxIndex) {
+            indexHandler.increment()
+        }
+    }
+
+    @Test
+    fun `test reset`() {
+        val maxIndex = 10
+        val indexHandler = LineIndexHandler(maxIndex)
+        for (i in 0 until maxIndex) {
+            indexHandler.increment()
+        }
+        indexHandler.reset()
+        Assert.assertEquals(indexHandler.index(), 0)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `test exception when construct`() {
+        val wrongIndex = -4
+        val indexHandler = LineIndexHandler(wrongIndex)
+    }
+}
