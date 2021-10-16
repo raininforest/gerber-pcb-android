ab0e16c65e0d0603648ac91a667a1211dfa96848
Sergey Velesko <s.velesko@yandex.ru>
2021-10-16 09:56:00 +0300

    created D01, D02 commands

---
+++ syntaxparser/src/main/java/com/github/raininforest/syntaxparser/impl/commands/operations/D01Command.kt
@@ -0,0 +1,130 @@
+package com.github.raininforest.syntaxparser.impl.commands.operations
+
+import com.github.raininforest.core.radiansToDegrees
+import com.github.raininforest.syntaxparser.api.GerberCommand
+import com.github.raininforest.syntaxparser.api.GraphicsProcessor
+import com.github.raininforest.syntaxparser.api.PointD
+import com.github.raininforest.syntaxparser.api.graphicsstate.CoordinateFormat
+import com.github.raininforest.syntaxparser.api.graphicsstate.enums.InterpolationState
+import com.github.raininforest.syntaxparser.api.graphicsstate.enums.RegionMode
+import com.github.raininforest.syntaxparser.impl.CoordinateDataParsable
+import com.github.raininforest.syntaxparser.impl.exceptions.WrongCommandFormatException
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+/**
+ * Performs an interpolation operation, creating a draw or an arc segment.
+ * The interpolation state defines which type of segment is created.
+ *
+ * Created by Sergey Velesko on 19.09.2021
+ */
+data class D01Command(
+    private val x: Double? = null,
+    private val y: Double? = null,
+    private val i: Double = 0.0,
+    private val j: Double = 0.0,
+    override val lineNumber: Int
+) : DOperationCommand(), GerberCommand {
+
+    override fun perform(processor: GraphicsProcessor) {
+        when (processor.graphicsState.interpolationState) {
+            InterpolationState.LINEAR -> drawLine(processor)
+            InterpolationState.CLOCKWISE_CIRCULAR -> drawArc(processor, isClockwise = true)
+            InterpolationState.COUNTERCLOCKWISE_CIRCULAR -> drawArc(processor, isClockwise = false)
+        }
+    }
+
+    private fun drawLine(processor: GraphicsProcessor) {
+        if (processor.regionMode == RegionMode.REGION) {
+            processor.lineTo(
+                x = x ?: processor.graphicsState.currentPoint.x,
+                y = y ?: processor.graphicsState.currentPoint.y,
+            )
+        } else {
+            processor.drawLine(
+                x = x ?: processor.graphicsState.currentPoint.x,
+                y = y ?: processor.graphicsState.currentPoint.y
+            )
+        }
+        updateCurrentPoint(processor, x, y)
+    }
+
+    private fun drawArc(processor: GraphicsProcessor, isClockwise: Boolean) {
+        val center = PointD(
+            processor.graphicsState.currentPoint.x + i,
+            processor.graphicsState.currentPoint.y + j
+        )
+        val radius = sqrt((i * i) + (j * j))
+        val xVal = x ?: processor.graphicsState.currentPoint.x
+        val yVal = y ?: processor.graphicsState.currentPoint.y
+
+        val startAngle = atan2(-i, -j).radiansToDegrees()
+        val endAngle = atan2(yVal - center.y, xVal - center.x).radiansToDegrees()
+        val sweepAngle = calculateSweepAngle(startAngle, endAngle, isClockwise)
+
+        if (processor.regionMode == RegionMode.REGION) {
+            processor.arcTo(
+                left = center.x - radius,
+                top = center.y + radius,
+                right = center.x + radius,
+                bottom = center.y - radius,
+                startAngle = startAngle,
+                sweepAngle = sweepAngle
+            )
+        } else {
+            processor.drawArc(
+                left = center.x - radius,
+                top = center.y + radius,
+                right = center.x + radius,
+                bottom = center.y - radius,
+                startAngle = startAngle,
+                sweepAngle = sweepAngle
+            )
+        }
+    }
+
+    internal companion object : CoordinateDataParsable {
+
+        override fun parse(
+            currentString: String,
+            lineNumber: Int,
+            coordinateFormat: CoordinateFormat
+        ): GerberCommand {
+            val coordinates =
+                try {
+                    parseCoordinates(
+                        currentString,
+                        coordinateFormat.integerCount,
+                        coordinateFormat.decimalCount
+                    )
+                } catch (e: Throwable) {
+                    throw WrongCommandFormatException(lineNumber, e.localizedMessage)
+                }
+
+            return D01Command(
+                x = coordinates["X"],
+                y = coordinates["Y"],
+                i = coordinates["I"] ?: 0.0,
+                j = coordinates["J"] ?: 0.0,
+                lineNumber
+            )
+        }
+
+        private fun calculateSweepAngle(
+            startAngle: Double,
+            endAngle: Double,
+            isClockwise: Boolean
+        ) = when (isClockwise) {
+            true -> if (startAngle <= endAngle) {
+                endAngle - (startAngle + 360)
+            } else {
+                endAngle - startAngle
+            }
+            false -> if (startAngle >= endAngle) {
+                (endAngle + 360) - startAngle
+            } else {
+                endAngle - startAngle
+            }
+        }
+    }
+}
---
+++ syntaxparser/src/main/java/com/github/raininforest/syntaxparser/impl/commands/operations/D02Command.kt
@@ -0,0 +1,52 @@
+package com.github.raininforest.syntaxparser.impl.commands.operations
+
+import com.github.raininforest.syntaxparser.api.GerberCommand
+import com.github.raininforest.syntaxparser.api.GraphicsProcessor
+import com.github.raininforest.syntaxparser.api.graphicsstate.CoordinateFormat
+import com.github.raininforest.syntaxparser.api.graphicsstate.enums.RegionMode
+import com.github.raininforest.syntaxparser.impl.CoordinateDataParsable
+import com.github.raininforest.syntaxparser.impl.exceptions.WrongCommandFormatException
+
+/**
+ * Moves the current point to the ([x], [y])
+ *
+ * Created by Sergey Velesko on 19.09.2021
+ */
+data class D02Command(
+    private val x: Double? = null,
+    private val y: Double? = null,
+    override val lineNumber: Int
+) : DOperationCommand(), GerberCommand {
+
+    override fun perform(processor: GraphicsProcessor) {
+        if (processor.regionMode == RegionMode.REGION) {
+            processor.closeContourAndStartNewOne()
+        }
+        updateCurrentPoint(processor, x, y)
+    }
+
+    internal companion object : CoordinateDataParsable {
+        override fun parse(
+            currentString: String,
+            lineNumber: Int,
+            coordinateFormat: CoordinateFormat
+        ): GerberCommand {
+            val coordinates =
+                try {
+                    parseCoordinates(
+                        currentString,
+                        coordinateFormat.integerCount,
+                        coordinateFormat.decimalCount
+                    )
+                } catch (e: Throwable) {
+                    throw WrongCommandFormatException(lineNumber, e.localizedMessage)
+                }
+
+            return D02Command(
+                x = coordinates["X"],
+                y = coordinates["Y"],
+                lineNumber = lineNumber
+            )
+        }
+    }
+}
