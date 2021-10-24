9381f8760afd4b24f9d3349479a99df42e7f3131
Sergey Velesko <s.velesko@yandex.ru>
2021-10-24 11:56:47 +0300

    added logger dependency for syntax parser

--- syntaxparser/build.gradle.kts
+++ syntaxparser/build.gradle.kts
@@ -7,6 +7,8 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 dependencies {
+    implementation(project(mapOf("path" to ":logger")))
+
     testImplementation("io.mockk:mockk:1.11.0")
     implementation("junit:junit:${Versions.junit}")
     implementation(project(mapOf("path" to ":core")))
