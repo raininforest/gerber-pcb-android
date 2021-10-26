d745aa34f8b5cb4c140513bdc2d5f7b9f0f2fe4a
Sergey Velesko <s.velesko@yandex.ru>
2021-10-26 19:09:23 +0300

    covered valueOrZero extension by unit test

--- syntaxparser/src/main/java/com/github/raininforest/syntaxparser/impl/utils/CollectionExtensions.kt
+++ syntaxparser/src/main/java/com/github/raininforest/syntaxparser/impl/utils/CollectionExtensions.kt
@@ -9,4 +9,4 @@ package com.github.raininforest.syntaxparser.impl.utils
 /**
  * Get value with specified [index] or returns 0.0, if no such index
  */
-fun List<Double>.valueOrZero(index: Int): Double = if (index < size) this[index] else 0.0
+internal fun List<Double>.valueOrZero(index: Int): Double = if (index < size) this[index] else 0.0
