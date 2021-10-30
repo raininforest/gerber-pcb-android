ec1dc02c2c00b52831ccfe1d92610ea21dbff4d4
Sergey Velesko <s.velesko@yandex.ru>
2021-10-30 20:41:48 +0300

    made var CoordinateFormat properties

--- syntaxparser/src/main/java/com/github/raininforest/syntaxparser/api/graphicsstate/CoordinateFormat.kt
+++ syntaxparser/src/main/java/com/github/raininforest/syntaxparser/api/graphicsstate/CoordinateFormat.kt
@@ -4,6 +4,6 @@ package com.github.raininforest.syntaxparser.api.graphicsstate
  * Created by Sergey Velesko on 19.09.2021
  */
 data class CoordinateFormat(
-    val integerCount: Int,
-    val decimalCount: Int
+    var integerCount: Int,
+    var decimalCount: Int
 )
