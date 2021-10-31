7260da349c897884ac142fde9a973952fbfa761b
Sergey Velesko <s.velesko@yandex.ru>
2021-10-31 13:03:47 +0300

    implemented LayersFragment, inject viewModel with koin

--- app/src/main/java/com/github/raininforest/gerberpcb/ui/layers/LayersFragment.kt
+++ app/src/main/java/com/github/raininforest/gerberpcb/ui/layers/LayersFragment.kt
@@ -6,46 +6,42 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.github.raininforest.gerberpcb.R
 import com.github.raininforest.gerberpcb.databinding.LayersFragmentBinding
-import com.github.raininforest.gerberpcb.ui.showMsg
+import com.github.raininforest.gerberpcb.ui.showSnack
 import com.github.raininforest.logger.Logger
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
+ * [Fragment] for screen with list of gerbers
+ *
  * Created by Sergey Velesko on 18.07.2021
  */
 class LayersFragment : Fragment(R.layout.layers_fragment) {
 
     private val binding by viewBinding(LayersFragmentBinding::bind)
     private val listAdapter = GerberListAdapter()
-    private val viewModel: LayersViewModel by viewModels()
+    private val layersViewModel: LayersViewModel by viewModel()
 
     private val openFileResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val data: Intent? = result.data
-            val uri: Uri = data?.data ?: Uri.EMPTY
-            //TODO pass uri to ViewModel
-            Logger.d("File opened: ${uri.path}")
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val uri: Uri = data?.data ?: Uri.EMPTY
+                layersViewModel.gerberAdded(uri)
+                Logger.d("File opened: ${uri.path}")
+            }
         }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initButtons()
         initRecycler()
-        observeLiveData()
-    }
-
-    private fun observeLiveData() {
-        viewModel.data().observe(
-            viewLifecycleOwner,
-            { screenState -> renderData(screenState) }
-        )
+        initViewModel()
     }
 
     private fun initButtons() {
@@ -65,23 +61,23 @@ class LayersFragment : Fragment(R.layout.layers_fragment) {
         binding.recyclerView.adapter = listAdapter
     }
 
-    private fun renderData(screenState: LayersScreenState) {
+    private fun initViewModel() {
+        layersViewModel.data.observe(viewLifecycleOwner) { screenState -> renderData(screenState) }
+        layersViewModel.isLoading.observe(viewLifecycleOwner) { showLoading(it) }
+        layersViewModel.list()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.loadingIndicator.isGone = !isLoading
+    }
+
+    private fun renderData(screenState: LayersScreenState) =
         when (screenState) {
-            is LayersScreenState.Loading -> {
-                screenState.progress.observe(viewLifecycleOwner) { value ->
-                    renderProgress(value)
-                }
-            }
             is LayersScreenState.Success -> {
                 listAdapter.setList(screenState.gerberList)
             }
             is LayersScreenState.Error -> {
-                showMsg(screenState.error)
+                showSnack(binding.root, screenState.error)
             }
         }
-    }
-
-    private fun renderProgress(value: String) {
-        //TODO
-    }
 }
