9390d963836a5bc256145b59d1f1541c226da0db
Sergey Velesko <s.velesko@yandex.ru>
2021-11-14 13:20:45 +0300

    covered SyntaxParserImpl by tests

---
+++ syntaxparser/src/test/java/common/SyntaxParserTest.kt
@@ -0,0 +1,45 @@
+package common
+
+import com.github.raininforest.syntaxparser.api.SyntaxParser
+import com.github.raininforest.syntaxparser.impl.GerberValidator
+import com.github.raininforest.syntaxparser.impl.SyntaxParserImpl
+import com.github.raininforest.syntaxparser.impl.utils.readTestFile
+import org.junit.Assert
+import org.junit.Test
+import java.nio.file.Path
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.pathString
+
+/**
+ * Test for [SyntaxParserImpl]
+ *
+ * Created by Sergey Velesko on 23.10.2021
+ */
+class SyntaxParserTest {
+
+    private val gerberValidator = GerberValidator()
+    private val syntaxParser: SyntaxParser = SyntaxParserImpl(gerberValidator)
+    private val workingDir: Path = Path.of("src/test/test_gerbers")
+
+    @Test
+    fun `should parse valid gerbers`() {
+        workingDir.listDirectoryEntries().filter{
+            !it.pathString.contains("notValidFile")
+        }.forEach { path ->
+            val list = readTestFile(path.toFile())
+            val cmdList = syntaxParser.parse(list)
+            Assert.assertTrue(cmdList.isNotEmpty())
+        }
+    }
+
+    @Test
+    fun `should return empty list when parse invalid gerber`() {
+        workingDir.listDirectoryEntries().filter{
+            it.pathString.contains("notValidFile")
+        }.forEach { path ->
+            val list = readTestFile(path.toFile())
+            val cmdList = syntaxParser.parse(list)
+            Assert.assertTrue(cmdList.isEmpty())
+        }
+    }
+}
