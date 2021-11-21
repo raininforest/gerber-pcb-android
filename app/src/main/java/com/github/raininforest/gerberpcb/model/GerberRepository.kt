18146c17263c059016d3e677594c4a6272c32a08
Sergey Velesko <s.velesko@yandex.ru>
2021-11-21 10:41:34 +0300

    improved GerberRepository contract

--- app/src/main/java/com/github/raininforest/gerberpcb/model/GerberRepository.kt
+++ app/src/main/java/com/github/raininforest/gerberpcb/model/GerberRepository.kt
@@ -8,8 +8,8 @@ import android.net.Uri
  * Created by Sergey Velesko on 18.07.2021
  */
 interface GerberRepository {
-    suspend fun list(): List<GerberItemUi>
-    suspend fun addItem(filename: Uri): Boolean
-    suspend fun removeItem(id: String)
-    suspend fun changeItemVisibility(id: String, visibility: Boolean)
+    val gerbers: List<Gerber>
+    suspend fun addItem(fileUri: Uri): Boolean
+    fun removeItem(id: String)
+    fun changeItemVisibility(id: String, visibility: Boolean)
 }
