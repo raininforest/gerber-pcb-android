346daef9811813ca8d76e6be77a5b0d8cff0af28
Sergey Velesko <s.velesko@yandex.ru>
2021-11-23 20:11:35 +0300

    injected everything for graphics processor

--- app/src/main/java/com/github/raininforest/gerberpcb/di/KoinModules.kt
+++ app/src/main/java/com/github/raininforest/gerberpcb/di/KoinModules.kt
@@ -1,5 +1,8 @@
 package com.github.raininforest.gerberpcb.di
 
+import com.github.raininforest.GraphicsProcessor
+import com.github.raininforest.GraphicsProcessorImpl
+import com.github.raininforest.commandprocessor.CommandProcessorImpl
 import com.github.raininforest.gerberfilereader.GerberFileReader
 import com.github.raininforest.gerberfilereader.GerberFileReaderImpl
 import com.github.raininforest.gerberpcb.app.logger.GerberReaderLogger
@@ -8,6 +11,7 @@ import com.github.raininforest.gerberpcb.model.GerberRepository
 import com.github.raininforest.gerberpcb.model.GerberRepositoryImpl
 import com.github.raininforest.gerberpcb.ui.screens.layersscreen.LayersViewModel
 import com.github.raininforest.logger.ILogger
+import com.github.raininforest.syntaxparser.api.CommandProcessor
 import com.github.raininforest.syntaxparser.api.SyntaxParser
 import com.github.raininforest.syntaxparser.impl.GerberValidator
 import com.github.raininforest.syntaxparser.impl.SyntaxParserImpl
@@ -25,9 +29,9 @@ val application = module {
     single<GerberFileReader> { GerberFileReaderImpl(context = get()) }
     single { GerberValidator() }
     single<SyntaxParser> { SyntaxParserImpl(gerberValidator = get()) }
-
-    single { GerberProcessor(fileReader = get(), parser = get()) }
-
+    factory<CommandProcessor> { CommandProcessorImpl() }
+    single<GraphicsProcessor> { GraphicsProcessorImpl(commandProcessor = get()) }
+    single { GerberProcessor(fileReader = get(), parser = get(), graphicsProcessor = get()) }
     single<GerberRepository> { GerberRepositoryImpl(gerberProcessor = get()) }
 
     viewModel { LayersViewModel(gerberRepository = get()) }
