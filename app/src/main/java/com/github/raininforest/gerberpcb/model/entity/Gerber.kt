0f5ce92bf8f4613282b07602c0e7f8265a6aaad5
Sergey Velesko <s.velesko@yandex.ru>
2021-11-28 12:58:47 +0300

    added color field to Gerber entity

--- app/src/main/java/com/github/raininforest/gerberpcb/model/entity/Gerber.kt
+++ app/src/main/java/com/github/raininforest/gerberpcb/model/entity/Gerber.kt
@@ -9,6 +9,11 @@ import java.util.*
 data class Gerber(
     val id: String = UUID.randomUUID().toString(),
     val name: String,
+    val color: String = DEFAULT_COLOR,
     var isVisible: Boolean = false,
     val data: List<GraphicsObject>
-)
+) {
+    companion object {
+        private const val DEFAULT_COLOR = "#ffffff"
+    }
+}
