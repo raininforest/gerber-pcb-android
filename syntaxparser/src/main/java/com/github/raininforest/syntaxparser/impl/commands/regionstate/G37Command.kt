556f98195cc7c5b28bcddeefb3a7e961d8bdd326
Sergey Velesko <s.velesko@yandex.ru>
2021-11-29 22:10:38 +0300

    fixed G37Command + added kdoc

--- syntaxparser/src/main/java/com/github/raininforest/syntaxparser/impl/commands/regionstate/G37Command.kt
+++ syntaxparser/src/main/java/com/github/raininforest/syntaxparser/impl/commands/regionstate/G37Command.kt
@@ -5,11 +5,14 @@ import com.github.raininforest.syntaxparser.api.CommandProcessor
 import com.github.raininforest.syntaxparser.api.graphicsstate.enums.RegionMode
 
 /**
+ * Finishes region statement
+ *
  * Created by Sergey Velesko on 19.09.2021
  */
 data class G37Command(override val lineNumber: Int) : GerberCommand {
 
     override fun perform(processor: CommandProcessor) {
+        processor.closeContour()
         processor.regionMode = RegionMode.NO_REGION
     }
 }
