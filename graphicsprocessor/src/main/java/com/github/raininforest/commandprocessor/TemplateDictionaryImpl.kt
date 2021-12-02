552b08033ca226aca14ce87ccc819196cad88546
Sergey Velesko <s.velesko@yandex.ru>
2021-12-02 22:28:57 +0300

    fixed base template type (replaced MacroTemplate by ApertureTemplate)

--- graphicsprocessor/src/main/java/com/github/raininforest/commandprocessor/TemplateDictionaryImpl.kt
+++ graphicsprocessor/src/main/java/com/github/raininforest/commandprocessor/TemplateDictionaryImpl.kt
@@ -1,18 +1,18 @@
 package com.github.raininforest.commandprocessor
 
 import com.github.raininforest.syntaxparser.api.dictionary.MacroTemplateDictionary
-import com.github.raininforest.syntaxparser.impl.commands.aperturemacro.templates.MacroTemplate
+import com.github.raininforest.syntaxparser.impl.commands.aperturemacro.templates.ApertureTemplate
 
 /**
  * [MacroTemplateDictionary] implementation
  */
 class TemplateDictionaryImpl : MacroTemplateDictionary {
-    private val templates = mutableMapOf<String, MacroTemplate>()
+    private val templates = mutableMapOf<String, ApertureTemplate>()
 
-    override fun add(item: MacroTemplate) {
+    override fun add(item: ApertureTemplate) {
         templates[item.name] = item
     }
 
-    override fun get(id: String): MacroTemplate = templates[id]
+    override fun get(id: String): ApertureTemplate = templates[id]
         ?: throw IllegalStateException("ERROR! No such aperture with id:$id in aperture dictionary!")
 }
